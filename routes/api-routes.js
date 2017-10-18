341e057df7476f9dd8bcba0089c7f0ab9836364e
Nicole Carvalho <NicoleCarvalho@Nicole-Carvalhos-MacBook-Pro.local>
2017-10-18 18:49:22 -0400

    adding in the most recent project with ray's changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,39 +1,109 @@
 var db = require("../models");
+var passport = require("../config/passport");
 var fs = require("fs");
 var surveyQs = require("../data/surveyQuestions");
 
 module.exports = function(app) {
-  //select - from the food table interfaced by the Food model
-  app.get("/", function(req, res) {
-    res.render("index");
-  });
 
-  app.get("/usersurvey", function(req, res) {
-    //questions is currently coming from a js data file, change here if questions will be stored in the database
-    var hbsObject = {questions:surveyQs};
-    console.log(hbsObject);
-    res.render("usersurvey", hbsObject);
-  });
 
-  app.get("/finddog", function(req, res) {
+app.post("/api/filter", function(req, res) {
+  var breed = req.body.breed;
+  var stringBreed = breed.join("+");
+  res.redirect("/finddog/" + stringBreed);
+});
 
-    // res.render("index");
-    db.Dog.findAll({}).then(function(result) {
-      // console.log(res.json(result));
 
-      var hbsObject = {dogdata:[]};
-      //an array referenced by a single JSON property in this case "foods") is expected over in HB
-      //so it is constructed in the loop below. The callback result is too rich with meta data so it needed to be pared down.
-      for (var i=0; i < result.length; i++) {
-        hbsObject.dogdata.push(result[i].dataValues);
-      }
-      console.log(hbsObject);
-      res.render("finddog", hbsObject);
+
+
+  app.post("/api/new", function(req, res) {
+    db.Dog.create({
+      owner_name: req.body.name,
+      breed: req.body.breed,
+      location: req.body.zip,
+      shedding: req.body.q4,
+      energy: req.body.q5,
+      trainability: req.body.q6,
+      kid: req.body.q7,
+      groom: req.body.q8,
+      drool: req.body.q9,
+      bark: req.body.q10,
+      independence: req.body.q11,
+      weight: req.body.q12,
+      image: req.body.imagepath
+    }).then(function(newDog) {
+      res.json({id:newDog.dataValues.id});
     });
   });
 
+
+app.post("/api/photo/", function (req, res) {
+    if (!req.files) return res.status(400).send('No files were uploaded!');
+
+    let currentDogImage = req.files.userPhoto;
+    var randomNum = Math.floor(Math.random() * 50000);
+    var filename = randomNum + req.files.userPhoto.name;
+    currentDogImage.mv('./public/uploads/' + filename, function (err) {
+      res.send(filename);
+    });
+  });
+
+
+app.put("/api/dogs/:id", function(req, res) {
+  db.Dog.update({
+      treats: req.body.treats
+    }, {
+      where: {
+        id: req.params.id
+      }
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+app.post('/api/newOwner', function(req, res) {
+  db.OwnerData.create({
+    username: req.body.uname,
+    password: req.body.psw, 
+    firstname: req.body.fn,
+    lastname: req.body.ln,
+    email: req.body.email,
+    address: req.body.address
+     }).then(function() {
+      res.redirect("/login");
+    }).catch(function(err) {
+      console.log(err);
+      res.json(err);
+      // res.status(422).json(err.errors[0].message);
+    });
+  });
+
+  app.get("/api/login", passport.authenticate("local"), function(req, res) {
+      res.redirect("/listdog");
+  });
+
+   app.get("/api/user_data", function(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    }
+    else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        username: req.user.username,
+        id: req.user.id
+      });
+    }
+  });
+
+   app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
+
   //new profile
-  app.get("/newprofile/:id", function (req, res) {
+  app.get("/newprofile/:id", passport.authenticate("local"), function (req, res) {
     var id = req.params.id;
     db.Dog.findAll({where:{id:id}}).then(function(result) {
       var hbsObject = {dogdata:[]};
@@ -61,54 +131,8 @@ module.exports = function(app) {
 
   });
 
-  app.get("/listdog", function(req, res) {
-      res.render("listdog");
-  });
 
-  app.post("/api/new", function(req, res) {
-    db.Dog.create({
-      owner_name: req.body.name,
-      breed: req.body.breed,
-      location: req.body.zip,
-      shedding: req.body.q4,
-      energy: req.body.q5,
-      trainability: req.body.q6,
-      kid: req.body.q7,
-      groom: req.body.q8,
-      drool: req.body.q9,
-      bark: req.body.q10,
-      independence: req.body.q11,
-      weight: req.body.q12,
-      image: req.body.imagepath
-    }).then(function(newDog) {
-      res.json({id:newDog.dataValues.id});
-    });
-  });
-
-  app.post("/api/photo/", function (req, res) {
-    if (!req.files) return res.status(400).send('No files were uploaded!');
-
-    let currentDogImage = req.files.userPhoto;
-    var randomNum = Math.floor(Math.random() * 50000);
-    var filename = randomNum + req.files.userPhoto.name;
-    currentDogImage.mv('./public/uploads/' + filename, function (err) {
-      res.send(filename);
-    });
-  });
-
-  app.put("/api/dogs/:id", function(req, res) {
-    db.Dog.update({
-      treats: req.body.treats
-    }, {
-      where: {
-        id: req.params.id
-      }
-    }).then(function(result) {
-      res.json(result);
-    });
-  });
-
-  app.post('/api/newsurvey', function(req, res) {
+app.post('/api/newsurvey', function(req, res) {
     var respondentData = req.body;
     var respondentArray = [];
     var storeArray = [];
@@ -184,19 +208,6 @@ module.exports = function(app) {
 app.get("/api/getsurvquestions", function (req, res) {
   res.json(surveyQs);
 });
-  //update
-  // app.put("/api/foods/:id", function(req,res) {
-  //   db.Food.update({
-  //     name: req.body.name,
-  //     eaten: req.body.eaten
-  //   }, {
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(result) {
-  //     res.json(result);
-  //   });
-  // });
 
   //delete
   app.delete("/api/deletedog/:id", function(req, res) {
@@ -209,4 +220,7 @@ app.get("/api/getsurvquestions", function (req, res) {
     });
   });
 
+
+
 };
+
