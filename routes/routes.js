91a94df1b65c593e470e2a248dbe71bb4c9d77a6
Nicole Carvalho <NicoleCarvalho@Nicole-Carvalhos-MacBook-Pro.local>
2017-10-18 14:44:19 -0400

    filter logic added

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 var fs = require("fs");
 
 module.exports = function(app) {
-  //select - from the food table interfaced by the Food model
+
   app.get("/", function(req, res) {
     res.render("index");
   });
@@ -11,26 +11,43 @@ module.exports = function(app) {
     res.render("usersurvey");
   });
 
-  app.get("/finddog", function(req, res) {
 
-    // res.render("index");
-    db.Dog.findAll({}).then(function(result) {
-      // console.log(res.json(result));
+app.get("/finddog/:breed?", function(req, res) {
+  var dogBreed = req.params.breed;
+  if(dogBreed == undefined) {
+    dogBreed = ["basset hound", "beagle", "boxer", "bull dog", "dalmation", "doberman", "cockapoo", "collie", "foxhound", "german shepherd", "golden retriever", "goldendoodle", "greyhound", "maltese", "mutt", "pit bull", "pomeranian", "poodle", "portuguese water dog", "pug", "saint bernard", "shih tzu", "siberian husky", "welsh terrier", "yorkshire terrier"]
+  } else {
+    dogBreed = dogBreed.split("+");
+  }
 
+  db.Dog.findAll({
+    where: {
+      breed: dogBreed
+    }
+  }).then(function(result){
       var hbsObject = {dogdata:[]};
       //an array referenced by a single JSON property in this case "foods") is expected over in HB
       //so it is constructed in the loop below. The callback result is too rich with meta data so it needed to be pared down.
       for (var i=0; i < result.length; i++) {
         hbsObject.dogdata.push(result[i].dataValues);
       }
-      console.log(hbsObject);
-      res.render("finddog", hbsObject);
-    });
+
+    res.render("finddog", hbsObject);
+
   });
 
-  app.get("/listdog", function(req, res) {
-      res.render("listdog");
-  });
+});
+
+app.post("/api/filter", function(req, res) {
+  var breed = req.body.breed;
+  var stringBreed = breed.join("+");
+  res.redirect("/finddog/" + stringBreed);
+
+});
+
+app.get("/listdog", function(req, res) {
+  res.render("listdog");
+});
 
 
 var filename;
@@ -51,7 +68,7 @@ var filename;
       weight: req.body.q12,
       image: filename
     }).then(function(newDog) {
-      res.redirect("/finddog"); 
+      res.send(res); 
     });
   });
 
@@ -63,16 +80,18 @@ app.post("/api/photo/", function(req, res) {
  
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let currentDogImage = req.files.userPhoto;
-  filename = req.files.userPhoto.name;
+  var randomNum = Math.floor(Math.random() * 50000);
+  filename = randomNum + req.files.userPhoto.name;
   console.log(filename);
   console.log(currentDogImage);
  
   // Use the mv() method to place the file somewhere on your server
-  currentDogImage.mv('./uploads/' + filename, function(err) {
+  currentDogImage.mv('./public/uploads/' + filename, function(err) {
  
-    res.redirect("/listdog");
+    res.send(filename);
   });
 });
+
 
 // var multer = require('multer');
 // var storage =  multer.diskStorage({
@@ -109,7 +128,19 @@ app.put("/api/dogs/:id", function(req, res) {
     });
   });
 
-app.post('/api/newsurvey',function(req,res){
+
+var surveyScoreArray = [];
+app.post("/api/newsurvey",function(req,res){
+
+    surveyScoreArray.push(req.body.q3);
+    surveyScoreArray.push(req.body.q4);
+    surveyScoreArray.push(req.body.q5);
+    surveyScoreArray.push(req.body.q6);
+    surveyScoreArray.push(req.body.q7);
+    surveyScoreArray.push(req.body.q8);
+    surveyScoreArray.push(req.body.q9);
+    surveyScoreArray.push(req.body.q10);
+    surveyScoreArray.push(req.body.q11);
 
     db.User.create({
       name: req.body.name,
@@ -124,53 +155,128 @@ app.post('/api/newsurvey',function(req,res){
       independence: req.body.q10,
       weight: req.body.q11
     }).then(function(newSurvey) {
-      res.redirect("/finddog"); 
+      res.redirect("/api/new"); 
     });
 
 });
 
 
-  // //dormant api
-  // app.get("/api/foods/:id", function(req, res) {
-  //   db.Food.findOne({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(result) {
-  //     res.json(result);
-  //   });
-  // });
 
-  // //insert - req.body is created as JSON object passed over on the client side
-  // app.post("/api/foods", function(req, res) {
-  //   db.Food.create(req.body).then(function(result) {
-  //     res.json(result);
-  //   });
-  // });
+var allDogsObjs = [];
+var totalDiffArray = [];
+// Retrieve all dogs from the database
+// store each of their scores in an array
+// push all 
 
-  //update
-  // app.put("/api/foods/:id", function(req,res) {
-  //   db.Food.update({
-  //     name: req.body.name,
-  //     eaten: req.body.eaten
-  //   }, {
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(result) {
-  //     res.json(result);
-  //   });
-  // });
+if (surveyScoreArray !== []) { 
+app.get("/api/new", function(req, res) { 
 
-  // //delete
-  // app.delete("/api/foods/:id", function(req, res) {
-  //   db.Food.destroy({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(result) {
-  //     res.json(result);
-  //   });
-  // });
+ db.Dog.findAll({}).then(function(result) {
+
+    var dogObject = {dogdata:[]};
+      //an array referenced by a single JSON property in this case "foods") is expected over in HB
+      //so it is constructed in the loop below. The callback result is too rich with meta data so it needed to be pared down.
+      for (var i=0; i < result.length; i++) {
+        dogObject.dogdata.push(result[i].dataValues);
+      }
+      console.log(dogObject);
+
+      var dogArray = [];
+      for (var i = 0; i < dogObject.dogdata.length; i++) {
+        var diffArray = [];
+        var dogScores = {
+          dogName: dogObject.dogdata[i].owner_name,
+          score: []
+        };
+        var shedding = dogObject.dogdata[i].shedding;
+        var energy = dogObject.dogdata[i].energy;
+        var trainability = dogObject.dogdata[i].trainability;
+        var kid = dogObject.dogdata[i].kid;
+        var groom = dogObject.dogdata[i].groom;
+        var drool = dogObject.dogdata[i].drool;
+        var bark = dogObject.dogdata[i].bark;
+        var independence = dogObject.dogdata[i].independence;
+        var weight = dogObject.dogdata[i].weight;
+
+        dogScores.score.push(shedding);
+        dogScores.score.push(energy);
+        dogScores.score.push(trainability);
+        dogScores.score.push(kid);
+        dogScores.score.push(groom);
+        dogScores.score.push(drool);
+        dogScores.score.push(bark);
+        dogScores.score.push(independence);
+        dogScores.score.push(weight);
+
+        dogArray.push(dogScores);
+      }
+      console.log(surveyScoreArray);
+      console.log(dogScores);
+      console.log(dogArray);
+
+      var count = 0;
+
+      function checkCount() {
+        if (count < dogObject.dogdata.length) {
+          console.log("Number of Dogs in database: " + dogObject.dogdata.length);
+          compareDogs();
+        }
+      }
+      checkCount();
+
+      function compareDogs() { 
+
+        var scoreDiffArray = [];
+        var currentDogScore = dogArray[count].score;
+
+        for (var i = 0; i < currentDogScore.length; i++) {
+          for (var i = 0; i < surveyScoreArray.length; i++) {
+           var diff = parseInt(surveyScoreArray[i]) - parseInt(currentDogScore[i]);
+           scoreDiffArray.push(Math.abs(diff));
+          }
+
+          function getSum(total, num) {
+            return total + num;
+          }
+
+          var totalDiff = scoreDiffArray.reduce(getSum);
+          totalDiffArray.push(totalDiff);
+          count++;
+          console.log("Score Diff Array: " + scoreDiffArray);
+          console.log("Total Diff: " + totalDiff);
+          checkCount();
+        }
+
+      }
+
+      minNumber = Math.min( ...totalDiffArray );
+      console.log("Lowest Number: " + minNumber);
+
+      for (var i = 0; i < totalDiffArray.length; i++) {
+        if(totalDiffArray[i] === minNumber) {
+          var index = totalDiffArray.indexOf(totalDiffArray[i]);
+
+          var match = {
+            matchScore: minNumber,
+            index: index,
+            name: dogObject.dogdata[index].owner_name,
+            breed: dogObject.dogdata[index].breed,
+            pic: dogObject.dogdata[index].image
+          }
+        }
+      }
+
+      matchString = JSON.stringify(match);
+
+      console.log("MATCH: " + matchString);
+
+      res.render("match", match);
+
+
+      });
+
+  });
+}
 
 };
+
