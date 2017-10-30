3f1fe870b434d457f2e16667d7f14c34153dc994
Nicole Carvalho <NicoleCarvalho@Nicole-Carvalhos-MacBook-Pro.local>
2017-10-30 14:57:40 -0400

    pushinghing changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -37,7 +37,7 @@ module.exports = function(app) {
 
     let currentDogImage = req.files.userPhoto;
     var filename = req.files.userPhoto.name;
-    currentDogImage.mv('https://s3.amazonaws.com/bucketname/' + filename, function (err) {
+    currentDogImage.mv('http://dogspotter-aws-bucket.s3.amazonaws.com/' + filename, function (err) {
       res.send(filename);
     });
   });
