3d88c01406ac91bbbda34a354af1aab3a1135452
Nicole Carvalho <NicoleCarvalho@NicoleCalhosMBP.home>
2017-10-30 16:10:37 -0400

    pushing change

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -37,7 +37,7 @@ module.exports = function(app) {
 
     let currentDogImage = req.files.userPhoto;
     var filename = req.files.userPhoto.name;
-    currentDogImage.mv('uploads/' + filename, function (err) {
+    currentDogImage.mv('/uploads/' + filename, function (err) {
       res.send(filename);
     });
   });
